c1360414fc0ce26b99689d9e67bb86bc88482b57
ybiquitous <10koba01@gmail.com>
2015-11-25 16:40:49 +0900

    Implemented `DeepModel#set` method

--- test/backbone.deepmodel_spec.js
+++ test/backbone.deepmodel_spec.js
@@ -1,7 +1,16 @@
-/*eslint-env mocha */
+/* eslint-env mocha */
+/* global should, sinon */
 import DeepModel from '../lib/backbone.deepmodel';
 
 describe('Backbone.DeepModel', () => {
+
+  let sandbox;
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
 
   it('extends custom class by `extend` static method', () => {
     let Model = DeepModel.extend({
@@ -24,12 +33,77 @@ describe('Backbone.DeepModel', () => {
     new Model().initialized.should.be.true;
   });
 
-  it('gets nested attributes', () => {
+  it('gets attributes', () => {
     class Model extends DeepModel {}
-    let model = new Model({a: 1, b: {x: '*'}});
+    let model = new Model({a: 1, b: {x: '*', y: [true, false]}});
     model.get('a').should.equal(1);
-    model.get('b').should.deep.equal({x: '*'});
+    model.get('b').should.deep.equal({x: '*', y: [true, false]});
     model.get('b.x').should.equal('*');
+    model.get('b.y').should.deep.equal([true, false]);
+    model.get('b.y[0]').should.equal(true);
+    model.get('b.y[1]').should.equal(false);
+    should.equal(model.get('b.y[2]'), undefined);
+    should.equal(model.get('c'), undefined);
+    should.equal(model.get('b.z'), undefined);
+  });
+
+  it('sets simple attribute', () => {
+    class Model extends DeepModel {}
+    let model = new Model();
+    let change = sandbox.spy();
+    let changeA = sandbox.spy();
+    model.on('change', change);
+    model.on('change:a', changeA);
+
+    let options = {};
+    model.set('a', 1, options);
+    model.get('a').should.equal(1);
+
+    model.set({'a': 2}, options);
+    model.get('a').should.equal(2);
+
+    change.callCount.should.equal(2);
+    change.args.should.deep.equal([
+      [model, options],
+      [model, options]
+    ]);
+    changeA.callCount.should.equal(2);
+    changeA.args.should.deep.equal([
+      [model, 1, options],
+      [model, 2, options]
+    ]);
+  });
+
+  it('sets nested attribute', () => {
+    class Model extends DeepModel {}
+    let model = new Model({a: {b: {c: null}}});
+    let change = sandbox.spy();
+    let changeA = sandbox.spy();
+    model.on('change', change);
+    model.on('change:a', changeA);
+
+    let options = {};
+    model.set('a.b.c', 1, options);
+    model.get('a.b.c').should.equal(1);
+
+    model.set({'a.b.c': 2}, options);
+    model.get('a.b.c').should.equal(2);
+
+    model.set('a.b', {c: 3}, options);
+    model.get('a.b.c').should.equal(3);
+
+    change.callCount.should.equal(3);
+    change.args.should.deep.equal([
+      [model, options],
+      [model, options],
+      [model, options]
+    ]);
+    changeA.callCount.should.equal(3);
+    changeA.args.should.deep.equal([
+      [model, {b: {c: 1}}, options],
+      [model, {b: {c: 2}}, options],
+      [model, {b: {c: 3}}, options]
+    ]);
   });
 
 });
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['es2015']
+          presets: ['es2015'],
+          plugins: ['add-module-exports']
         }
       }
     ]
