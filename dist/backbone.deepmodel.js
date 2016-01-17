aa7c61c93dbf5501d49e1947becf19cfe7651d65
ybiquitous <10koba01@gmail.com>
2016-01-17 17:04:13 +0900

    Use `for..of` loop, instead of normal `for` loop

--- dist/backbone.deepmodel.js
+++ dist/backbone.deepmodel.js
@@ -272,14 +272,35 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }
 
 	    var value = obj;
-	    for (var i = 0, len = pathElements.length; i < len; i++) {
-	      var pathElement = pathElements[i];
-	      if (pathElement in value) {
-	        value = value[pathElement];
-	      } else {
-	        return;
+	    var _iteratorNormalCompletion = true;
+	    var _didIteratorError = false;
+	    var _iteratorError = undefined;
+
+	    try {
+	      for (var _iterator = pathElements[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+	        var pathElement = _step.value;
+
+	        if (pathElement in value) {
+	          value = value[pathElement];
+	        } else {
+	          return;
+	        }
+	      }
+	    } catch (err) {
+	      _didIteratorError = true;
+	      _iteratorError = err;
+	    } finally {
+	      try {
+	        if (!_iteratorNormalCompletion && _iterator.return) {
+	          _iterator.return();
+	        }
+	      } finally {
+	        if (_didIteratorError) {
+	          throw _iteratorError;
+	        }
 	      }
 	    }
+
 	    return value;
 	  },
 
