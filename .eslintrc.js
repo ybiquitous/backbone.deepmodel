7092d443cde502ddc5e88cb1efd0fce87b512839
ybiquitous <ybiquitous@gmail.com>
2017-06-25 17:25:51 +0900

    chore(eslint): change format of eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'ybiquitous',
+  ],
+
+  env: {
+    mocha: true,
+  },
+
+  rules: {
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: [
+        'benchmark/*.js',
+        '*webpack*.js',
+        'test/**/*.js',
+      ],
+    }],
+  },
+}
