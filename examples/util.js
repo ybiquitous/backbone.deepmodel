c7067c7d90596a7ebb29b35eaa41e70d7ff482eb
ybiquitous <ybiquitous@gmail.com>
2017-06-25 16:59:13 +0900

    chore: yarn upgrade and fix eslint violations

--- examples/util.js
+++ examples/util.js
@@ -1,6 +1,8 @@
-/* eslint-disable no-console */
 const output = document.querySelector('.output')
+
+/* eslint-disable no-console */
 console.log = (arg) => {
   console.info(arg)
   output.innerHTML += `<pre><code>${JSON.stringify(arg)}</code></pre>`
 }
+/* eslint-enable no-console */
